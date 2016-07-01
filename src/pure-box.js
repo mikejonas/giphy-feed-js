ba1f623cd263276c43537443f20631233cbde983
mikejonas <motion@gmail.com>
2016-07-01 11:18:00 -0700

    Display next and prev buttons

--- src/pure-box.js
+++ src/pure-box.js
@@ -3,6 +3,8 @@
   this.PureBox = function(options, appContainer) {
     this.appContainer = appContainer || document.body;
     this.ClickHandlerInit();
+    this.photos = [];
+    this.currentPhoto;
   }
 
   this.PureBox.prototype.ClickHandlerInit = function() {
@@ -13,13 +15,19 @@
         element = element.parentElement;
       }
       if(element && element.matches('.pure-box')) {
-        this.launchLightBox(element.href, element.title);
+        this.openLightBox(element);
       }
     }.bind(this);
   }
 
-  this.PureBox.prototype.launchLightBox = function(imageUrl, title) {
-    this.createLightBoxElement(imageUrl, title);
+  this.PureBox.prototype.openLightBox = function(element) {
+    this.createLightBoxElement(element.href, element.title);
+    this.photos = this.appContainer.getElementsByClassName('pure-box');
+    this.currentPhotoIndex = Array.prototype.indexOf.call(this.photos, element);
+  }
+
+  this.PureBox.prototype.closeLightBox = function(element) {
+    element.parentElement.removeChild(element);
   }
 
   this.PureBox.prototype.createLightBoxElement = function(imageUrl, title) {
@@ -37,16 +45,41 @@
     overlayInnerDiv.appendChild(img);
     overlayInnerDiv.appendChild(captionDiv);
     document.body.insertBefore(overlayDiv, document.body.firstChild);
+    this.displayNextAndPreviousButtons(overlayInnerDiv);
     this.lightBoxClickHandler(overlayDiv)
+  }
+
+  this.PureBox.prototype.displayNextAndPreviousButtons = function(lightBoxElement) {
+    this.createNextPrevButton(lightBoxElement, '❬', 'prev');
+    this.createNextPrevButton(lightBoxElement, '❭', 'next');
+  }
+
+  this.PureBox.prototype.createNextPrevButton = function(lightBoxElement, arrowText, className) {
+    var arrowButton = document.createElement('a');
+    var arrowButtonInner = document.createElement('span');
+    arrowButtonInner.appendChild(document.createTextNode(arrowText));
+    arrowButton.className = 'next-prev ' + className;
+    arrowButtonInner.className = className;
+    arrowButton.appendChild(arrowButtonInner);
+    lightBoxElement.appendChild(arrowButton);
+    arrowButton.onClick = function(e) {
+
+    }
   }
 
   this.PureBox.prototype.lightBoxClickHandler = function(overlayDiv) {
     overlayDiv.onclick = function(e) {
-      var element = e.target;
-      if(element.matches('.pure-box-overlay')) {
-        this.parentElement.removeChild(this);
+      console.log(e.target);
+      if(e.target.matches('.pure-box-overlay')) {
+        this.closeLightBox(overlayDiv);
       }
-    };
+      if(e.target.matches('.pure-box-overlay')) {
+        this.closeLightBox(overlayDiv);
+      }
+      if(e.target.matches('.pure-box-overlay')) {
+        this.closeLightBox(overlayDiv);
+      }
+    }.bind(this);
   }
 
 }());
