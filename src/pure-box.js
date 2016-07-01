afee94e8a36fc3862b8d0460f14e94b6d7f42532
mikejonas <motion@gmail.com>
2016-07-01 00:19:41 -0700

    Basic lightbox functionality

--- src/pure-box.js
+++ src/pure-box.js
@@ -17,9 +17,31 @@
       }
     }.bind(this);
   }
-  
-  this.PureBox.prototype.launchLightBox = function(url, title) {
 
+  this.PureBox.prototype.launchLightBox = function(imageUrl, title) {
+    this.createLightBoxElement(imageUrl, title);
+  }
+
+  this.PureBox.prototype.createLightBoxElement = function(imageUrl, title) {
+    var overlayDiv = document.createElement('div')
+    var overlayInnerDiv = document.createElement('div')
+    var img = document.createElement('img')
+    overlayDiv.className = 'pure-box-overlay';
+    overlayInnerDiv.className = 'pure-box-overlay-inner';
+    img.src = imageUrl
+    overlayDiv.appendChild(overlayInnerDiv);
+    overlayInnerDiv.appendChild(img);
+    document.body.appendChild(overlayDiv);
+    this.lightBoxClickHandler(overlayDiv)
+  }
+
+  this.PureBox.prototype.lightBoxClickHandler = function(overlayDiv) {
+    overlayDiv.onclick = function(e) {
+      var element = e.target;
+      if(element.matches('.pure-box-overlay')) {
+        this.parentElement.removeChild(this);
+      }
+    };
   }
 
 }());
