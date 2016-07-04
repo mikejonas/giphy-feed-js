31440cb498f046e3212ad3c7bcaa62f12c85da40
mikejonas <motion@gmail.com>
2016-07-03 17:16:27 -0700

    Refactor and clean up code

--- src/giphy-feed.js
+++ src/giphy-feed.js
@@ -1,13 +1,12 @@
-(function () {
-
+(function() {
   this.GiphyFeed = function(options, appContainer) {
     this.appContainer = appContainer;
     this.apiKey = options.apiKey || 'dc6zaTOxFJmzC';
     this.photoMargin = options.photoMargin || 20;
     this.minColumnWidth = options.minColumnWidth || 300;
-    this.infiniteScroll = options.infiniteScroll === false ? false : true;
+    this.infiniteScroll = options.infiniteScroll;
     this.giphyApiOptions = options.giphyApiOptions;
-    this.searchTerm = options.searchTerm ? options.searchTerm.split(' ').join('+') : '';
+    this.searchTerm = options.searchTerm;
     this.photoData = [];
     this.columns = {
       elements: [],
@@ -18,22 +17,22 @@
     this.appendedPhotos = 0;
     this.initializeWindowEventHandlers();
     this.renderUI(true);
-  }
+  };
 
   this.GiphyFeed.prototype.renderUI = function(newSearch) {
     this.columns.heightRatios = [];
     this.columns.elements = [];
     this.appendedPhotos = 0;
     this.clearElements();
-    this.makeSearchForm();
+    this.makeSearchFormElement();
     this.makeColumnElements();
-    if(newSearch) {
+    if (newSearch) {
       this.photoData = [];
       this.getPhotos();
     }
-  }
+  };
 
-  this.GiphyFeed.prototype.makeSearchForm = function() {
+  this.GiphyFeed.prototype.makeSearchFormElement = function() {
     var form = document.createElement('form');
     var input = document.createElement('input');
     var button = document.createElement('button');
@@ -41,26 +40,26 @@
     form.style.marginLeft = this.photoMargin / 2 + 'px';
     form.style.marginRight = this.photoMargin / 2 + 'px';
     input.type = 'text';
-    button.className = 'giphy-search-button'
+    button.className = 'giphy-search-button';
 
-    button.appendChild(document.createTextNode('search'))
+    button.appendChild(document.createTextNode('search'));
     form.appendChild(input);
     form.appendChild(button);
-    input.value = this.giphyApiOptions.q
+    input.value = this.giphyApiOptions.q;
     this.appContainer.appendChild(form);
 
     form.onsubmit = function(e) {
-      if(this.giphyApiOptions.q !== input.value) {
+      if (this.giphyApiOptions.q !== input.value) {
         this.giphyApiOptions.q = input.value;
         this.renderUI(true);
       }
       return false;
     }.bind(this);
-  }
+  };
 
   this.GiphyFeed.prototype.makeColumnElements = function() {
     this.columns.count = Math.floor(this.appContainer.offsetWidth / this.minColumnWidth);
-    for(var i = 0; i < this.columns.count; i++) {
+    for (var i = 0; i < this.columns.count; i++) {
       var div = document.createElement('div');
       div.className = 'column';
       div.style.width = 100 / this.columns.count + '%';
@@ -68,98 +67,103 @@
       this.columns.heightRatios.push(0);
       this.appContainer.appendChild(div);
     }
-  }
+  };
 
   this.GiphyFeed.prototype.makePhotoElement = function(photoData, columnIndex) {
     var imageThumbnail = photoData.images.original_still;
     var imageFull = photoData.images.original;
-    var caption = extractHashTagsFromSlug(photoData.slug)
-    var div = document.createElement('div')
+    var caption = extractHashTagsFromSlug(photoData.slug);
+    var div = document.createElement('div');
     var a = document.createElement('a');
     var img = document.createElement('img');
 
-    div.className='photo-container';
+    div.className = 'photo-container';
     div.appendChild(a);
-    div.style.margin = '0 ' + this.photoMargin / 2 + 'px ' + this.photoMargin + 'px ' + this.photoMargin / 2 + 'px';;
+    div.style.margin = '0 ' + this.photoMargin / 2 + 'px ' + this.photoMargin + 'px ' + this.photoMargin / 2 + 'px';
     a.href = imageFull.url;
     a.title = caption;
-    a.className ='pure-box';
-    a.setAttribute('data-index', this.appendedPhotos++)
+    a.className = 'pure-box';
+    a.setAttribute('data-index', this.appendedPhotos++);
     a.appendChild(img);
     img.src = imageThumbnail.url;
     img.height = imageThumbnail.height;
     img.width = imageThumbnail.width;
 
-    this.columns.heightRatios[columnIndex] += parseInt(imageThumbnail.height) / parseInt(imageThumbnail.width);
+    this.columns.heightRatios[columnIndex] += parseInt(imageThumbnail.height, 10) / parseInt(imageThumbnail.width, 10);
     this.columns.elements[columnIndex].appendChild(div);
-  }
+  };
 
   this.GiphyFeed.prototype.getPhotos = function() {
     this.isLoadingImages = true;
     var giphyURl = buildGiphyUrl(this.apiKey, this.giphyApiOptions, this.appendedPhotos);
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        var data = JSON.parse(xmlHttp.responseText).data
+      if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+        var data = JSON.parse(xmlHttp.responseText).data;
         this.displayPhotos(data);
       }
-      if(xmlHttp.readyState == 4) {
+      if (xmlHttp.readyState === 4) {
         this.isLoadingImages = false;
       }
     }.bind(this);
     xmlHttp.open("GET", giphyURl, true); // true for asynchronous
     xmlHttp.send(null);
-  }
+  };
 
   this.GiphyFeed.prototype.displayPhotos = function(data) {
-    if (data) { this.photoData = this.photoData.concat(data) }
-    for(var i = this.appendedPhotos; i < this.photoData.length; i++) {
+    if (data) {
+      this.photoData = this.photoData.concat(data);
+    }
+    for (var i = this.appendedPhotos; i < this.photoData.length; i++) {
       var smallestColumnIndex = this.columns.heightRatios.indexOf(Math.min.apply(Math, this.columns.heightRatios));
       this.makePhotoElement(this.photoData[i], smallestColumnIndex);
     }
-  }
+  };
 
   this.GiphyFeed.prototype.clearElements = function() {
     while (this.appContainer.firstChild) {
       this.appContainer.removeChild(this.appContainer.firstChild);
     }
-  }
+  };
 
   this.GiphyFeed.prototype.initializeWindowEventHandlers = function() {
-    window.onresize = function(e) {
+    window.onresize = function() {
       var numberOfColumns = Math.floor(this.appContainer.offsetWidth / this.minColumnWidth);
-      if(this.columns.count !== numberOfColumns) {
+      if (this.columns.count !== numberOfColumns) {
         this.renderUI();
         var yOffset = window.pageYOffset;
         this.columns.count = numberOfColumns;
-        this.displayPhotos()
+        this.displayPhotos();
         window.scrollTo(0, yOffset);
       }
     }.bind(this);
 
-    if(this.infiniteScroll) {
-      window.onscroll = function(ev) {
+    if (this.infiniteScroll) {
+      window.onscroll = function() {
         if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight && !this.isLoadingImages) {
           this.getPhotos(this.appendedPhotos);
         }
       }.bind(this);
     }
+  };
 
-  }
-
-  //Helpers
+  // Helpers
   function extractHashTagsFromSlug(slug) {
-    var tags = slug.split('-')
+    var tags = slug.split('-');
     tags = tags.slice(0, tags.length - 1);
-    return tags.map(function(tag) {return '#' + tag }).join(' ')
+    return tags.map(function(tag) {
+      return '#' + tag;
+    }).join(' ');
   }
 
   function buildGiphyUrl(apiKey, options, offset) {
     var url = 'http://api.giphy.com/v1/gifs/search?api_key=' + apiKey;
-    for(var key in options) {
+    for (var key in options) {
       var value = options[key];
-      if(key === 'q') {
-        value = value ? options.q.split(' ').join('+') : '';
+      if (key === 'q') {
+        value = value ?
+          options.q.split(' ').join('+') :
+          '';
       }
       url += '&' + key + '=' + value;
     }
--- src/pure-box.js
+++ src/pure-box.js
@@ -1,68 +1,68 @@
-(function () {
-
+(function() {
   this.PureBox = function(options, appContainer) {
     this.appContainer = appContainer || document.body;
-    this.ClickHandlerInit();
     this.photos = [];
-    this.currentPhotoIndex;
-  }
+    this.currentPhotoIndex = 0;
+    this.initializeAppContainerClickHandler();
+  };
 
-  this.PureBox.prototype.ClickHandlerInit = function() {
+  this.PureBox.prototype.initializeAppContainerClickHandler = function() {
     this.appContainer.onclick = function(e) {
       var element = e.target;
-      while(element && !element.matches('.pure-box') && element !== this.appContainer) {
+      while (element && !element.matches('.pure-box') && element !== this.appContainer) {
         element = element.parentElement;
       }
-      if(element && element.matches('.pure-box')) {
-        e.preventDefault()
+      if (element && element.matches('.pure-box')) {
+        e.preventDefault();
         this.openLightBox(element);
       }
     }.bind(this);
-  }
+  };
 
   this.PureBox.prototype.openLightBox = function(element) {
-    this.createLightBoxElement(element.href, element.title);
+    this.createLightBoxElement(element);
     this.photos = Array.prototype.slice.call(this.appContainer.getElementsByClassName('pure-box'));
 
-    //Sort photos by data-index if available
-    if(element.dataset.index) {
-      this.photos = this.photos.sort(function(a,b) {
+    // Sort photos by data-index if available
+    if (element.dataset.index) {
+      this.photos = this.photos.sort(function(a, b) {
         return a.dataset.index - b.dataset.index;
       });
     }
     this.currentPhotoIndex = element.dataset.index || this.photos.indexOf(element);
-  }
+  };
 
   this.PureBox.prototype.closeLightBox = function(element) {
     element.parentElement.removeChild(element);
-    document.onkeydown = null
-  }
+    document.onkeydown = null;
+  };
 
-  this.PureBox.prototype.createLightBoxElement = function(imageUrl, title) {
-    var overlayDiv = document.createElement('div')
-    var overlayInnerDiv = document.createElement('div')
-    var img = document.createElement('div')
-    var captionDiv = document.createElement('div')
-    var captionText = document.createTextNode(title);
+  this.PureBox.prototype.createLightBoxElement = function(element) {
+    var overlayDiv = document.createElement('div');
+    var overlayInnerDiv = document.createElement('div');
+    var img = document.createElement('div');
+    var captionDiv = document.createElement('div');
+
+    captionDiv.className = 'pure-box-caption';
     overlayDiv.className = 'pure-box-overlay';
     overlayInnerDiv.className = 'pure-box-overlay-inner';
-    img.className = 'pure-box-image'
-    img.style.backgroundImage = "url('"+ imageUrl +"')"
-    captionDiv.className = 'pure-box-caption';
-    captionDiv.appendChild(captionText);
+    img.className = 'pure-box-image';
+    img.style.backgroundImage = "url('" + element.href + "')";
+
+    captionDiv.appendChild(document.createTextNode(element.title));
     overlayDiv.appendChild(overlayInnerDiv);
     overlayDiv.appendChild(captionDiv);
 
     overlayInnerDiv.appendChild(img);
     document.body.insertBefore(overlayDiv, document.body.firstChild);
     this.displayNextAndPreviousButtons(overlayInnerDiv);
-    this.lightBoxClickHandler(overlayDiv)
-  }
+    this.initializelightBoxEventHandlers(overlayDiv);
+  };
 
   this.PureBox.prototype.displayNextAndPreviousButtons = function(lightBoxElement) {
     this.createNextPrevButton(lightBoxElement, '❬', 'prev');
     this.createNextPrevButton(lightBoxElement, '❭', 'next');
-  }
+  };
 
   this.PureBox.prototype.createNextPrevButton = function(lightBoxElement, arrowText, className) {
     var arrowButton = document.createElement('a');
@@ -72,42 +72,51 @@
     arrowButtonInner.className = className;
     arrowButton.appendChild(arrowButtonInner);
     lightBoxElement.appendChild(arrowButton);
-  }
+  };
 
-  this.PureBox.prototype.lightBoxClickHandler = function(overlayDiv) {
+  this.PureBox.prototype.initializelightBoxEventHandlers = function(overlayDiv) {
     overlayDiv.onclick = function(e) {
-      if(e.target.matches('.pure-box-overlay') || e.target.matches('.pure-box-image')) {
+      if (e.target.matches('.pure-box-overlay') || e.target.matches('.pure-box-image')) {
         this.closeLightBox(overlayDiv);
       }
-      if(e.target.matches('.next') || e.target.matches('.prev')) {
+      if (e.target.matches('.next') || e.target.matches('.prev')) {
         this.navigateToImage(overlayDiv, e.target.matches('.next'));
-      } else {
-
       }
     }.bind(this);
 
     document.onkeydown = function(e) {
-      //37: left arrow. 39: right arrow. 27: escape key.
-      if(e.keyCode == '37' || e.keyCode == '39' || e.keyCode == '27') {
-        e.preventDefault()
+      // 37: left arrow. 39: right arrow. 27: escape key.
+      if (e.keyCode === 37 || e.keyCode === 39 || e.keyCode === 27) {
+        e.preventDefault();
       }
-      if (e.keyCode == '37') { this.navigateToImage(overlayDiv, false); }
-      if (e.keyCode == '39') { this.navigateToImage(overlayDiv, true); }
-      if (e.keyCode == '27') { this.closeLightBox(overlayDiv); }
+      if (e.keyCode === 37) {
+        this.navigateToImage(overlayDiv, false);
+      }
+      if (e.keyCode === 39) {
+        this.navigateToImage(overlayDiv, true);
+      }
+      if (e.keyCode === 27) {
+        this.closeLightBox(overlayDiv);
+      }
     }.bind(this);
-  }
+  };
 
   this.PureBox.prototype.navigateToImage = function(overlayDiv, goForward) {
-    goForward ? this.currentPhotoIndex++ : this.currentPhotoIndex--
+    this.currentPhotoIndex = goForward ?
+      this.currentPhotoIndex + 1 :
+      this.currentPhotoIndex - 1;
 
-    if(this.currentPhotoIndex > this.photos.length - 1) { this.currentPhotoIndex = 0; }
-    if(this.currentPhotoIndex < 0) { this.currentPhotoIndex = this.photos.length - 1; }
+    if (this.currentPhotoIndex > this.photos.length - 1) {
+      this.currentPhotoIndex = 0;
+    }
+    if (this.currentPhotoIndex < 0) {
+      this.currentPhotoIndex = this.photos.length - 1;
+    }
 
     var nextImage = this.photos[this.currentPhotoIndex];
     var image = overlayDiv.getElementsByClassName('pure-box-image')[0];
-    var caption = overlayDiv.getElementsByClassName('pure-box-caption')[0]
-    image.style.backgroundImage = "url('"+ nextImage.href +"')"
+    var caption = overlayDiv.getElementsByClassName('pure-box-caption')[0];
+    image.style.backgroundImage = "url('" + nextImage.href + "')";
     caption.innerText = nextImage.title;
-  }
-
+  };
 }());
